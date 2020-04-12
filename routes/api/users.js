ab0d0977ab20cc8714fed26cd7d1b3f9ec379d5d
aashnamahajan <mahajan.aashna@gmail.com>
2020-04-12 05:27:24 -0400

    creating models, adding express validations, adding gravatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,75 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
-// @route    GET api/users
-// @desc     Test route
+//get models
+const User = require("../../models/User");
+
+//for validations
+const { check, validationResult } = require("express-validator");
+
+// @route    POST api/users
+// @desc     Register user
 // @access   Public
-router.get("/", (req, res) => res.send("users route"));
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email address").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      //see if the user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      //get user's gravatar
+      const avatar = gravatar.url(email, {
+        s: "200", //size
+        r: "pg", //rating
+        d: "mm", //default
+      });
+
+      //creating instance of the user
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      //encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save(); // saving the user instance
+      //await is put infront of everything that gives a promise
+
+      //return jsonwebtoken
+
+      //when successful
+      res.send("users registered");
+    } catch (err) {
+      console.error(500).send("Server Error!!");
+    }
+  }
+);
 
 module.exports = router;
