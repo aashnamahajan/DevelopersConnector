f3c939aa537caf3f0b52fddd94b0277b449e5af9
aashnamahajan <mahajan.aashna@gmail.com>
2020-04-12 00:30:41 -0400

    connection with Database

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+//mongoose.connect(db);
+//instead of this above line we will use a better way where we get handle promises and exceptions
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true });
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.log(err.message);
+    //Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
