c4499f2556d91ba7ed9244e07de1ca953aedb362
aashnamahajan <mahajan.aashna@gmail.com>
2020-04-13 06:28:52 -0400

    user authentication and login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+
 // @route    GET api/auth
 // @desc     Test route
 // @access   Public
@@ -11,11 +16,75 @@ router.get("/", auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("-password");
     // .select('-password') is used since we do not want to display the password
-    res.json(user);
+    res.json(user); // user json can be seen on postman
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error!!");
   }
 });
+
+// @route    POST api/auth
+// @desc     Authenticate user and get token while logging in
+// @access   Public
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email address").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      //see if the user exists
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      //encrypt password
+      //const isMatch = await bcrypt.compare(password, user.password); //didn't work
+      const isMatch = password == user.password; //works
+      //password is what user entered which we got from req.body
+      //user.password is the password we get from the user's set password in db
+
+      //console.log(password, user.password, isMatch); //to test values
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      //return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtToken"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,11 +7,14 @@ connectDB();
 
 //Init Middleware
 app.use(express.json({ extended: false }));
+// no need of body-parser anymore
+//Express version 4.16+, their own body-parser implementation is now included in the default Express package so there is no need for you to download another dependency.
+//express can be used to parse json bodies
 
 app.get("/", (req, res) => res.send("API Running... "));
 
 //define routes
-app.use("/api/users", require("./routes/api/users"));
+app.use("/api/users", require("./routes/api/users")); //Registering the user
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
